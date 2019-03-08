3d38a5acc8e202f083d86e3d76bcedbd4635962b
Martin Angelov <m.angelov95@gmail.com>
2019-03-08 14:04:10 +0000

    Style modifications

--- app/src/main/java/com/bham/restaurantapp/ViewAllEstablishmentsActivity.java
+++ app/src/main/java/com/bham/restaurantapp/ViewAllEstablishmentsActivity.java
@@ -1,8 +1,12 @@
 package com.bham.restaurantapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -13,12 +17,11 @@ public class ViewAllEstablishmentsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_all_establishments);
-        ListView allEstablishmentsListView = findViewById(R.id.allEstablishmentsListView);
-        ArrayList<String> testing = new ArrayList<>();
-        testing.add("Haha");
-        testing.add("This");
-        testing.add("Actually");
-        testing.add("Works");
-        allEstablishmentsListView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_selectable_list_item, testing));
+//        ListView allEstablishmentsListView = findViewById(R.id.allEstablishmentsListView);
+        RecyclerView rView = findViewById(R.id.testRecyclerView);
+        rView.setHasFixedSize(true);
+        RecyclerView.LayoutManager lm = new LinearLayoutManager(this);
+        rView.setLayoutManager(lm);
+        // TODO: Implement RecyclerView to show all establishments
     }
 }
